ac3b4569005e09b4ad78cd89ccee4a7694959124
Sean Wernimont <swernimo@users.noreply.github.com>
2023-06-16 10:15:45 -0600

    bug fix

--- android/src/main/java/com/theblindsquirrel/opensettingsplugin/openappsettingsPlugin.java
+++ android/src/main/java/com/theblindsquirrel/opensettingsplugin/openappsettingsPlugin.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
 
-import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
@@ -16,10 +15,12 @@ public class openappsettingsPlugin extends Plugin {
 
     @PluginMethod
     public void open(PluginCall call) {
-        Activity act = getActivity();
-        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        act.startActivityForResult(intent, 3);
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getContext().getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(call, intent, 1);
+        call.resolve();
     }
 
     @PluginMethod
@@ -28,5 +29,6 @@ public class openappsettingsPlugin extends Plugin {
         Intent intent = new Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         act.startActivityForResult(intent, 3);
+        call.resolve();
     }
 }
